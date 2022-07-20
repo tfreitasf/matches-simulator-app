56e0868e69cb9f68229ffde4b3ce3c202378a3d7
tfreitasf <tfreitasf@gmail.com>
2022-07-19 21:37:59 -0300

    update file

---
+++ app/src/main/java/me/dio/simulador/MainActivity.java
@@ -0,0 +1,22 @@
+package me.dio.simulador;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import me.dio.simulador.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
